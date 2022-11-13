b2749fcb386138a36d1f272244033e7c55fda5db
Robin de Rooij <rderooij685@gmail.com>
2022-11-13 23:03:16 +0100

    Implement proper screen switching
    Now it is possible to select two screens at once

--- src/display.rs
+++ src/display.rs
@@ -1,49 +1,75 @@
 use std::process::Command;
 use std::str;
 
-use serde::{Deserialize};
+use serde::Deserialize;
 
+use std::collections::HashMap;
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Display {
     pub name: String,
     pub active: bool,
     pub modes: Vec<Mode>,
-    pub current_mode: Option<Mode>
+    pub current_mode: Option<Mode>,
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Deserialize, Clone, Debug)]
 pub struct Mode {
     pub width: u32,
     pub height: u32,
-    pub refresh: u32
+    pub refresh: u32,
 }
 
-pub fn get_display_modes() -> Vec<(Display, Mode)> {
-    let displays = get_displays();
-    let mut display_modes = Vec::new();
-    for display in displays {
-        for mode in &display.modes {
-            display_modes.push( (display.clone(), mode.clone()) )
+impl Display {
+    pub fn process(&self) {
+        self.onoff(self.active);
+        match &self.current_mode {
+            Some(mode) => self.set_output(mode.clone()),
+            None => (),
         }
     }
-    display_modes
+
+    pub fn set_output(&self, display_mode: Mode) {
+        Command::new("swaymsg")
+            .arg("output")
+            .arg(&self.name)
+            .arg("mode")
+            .arg(format!(
+                "{}x{}@{}Hz",
+                display_mode.width,
+                display_mode.height,
+                display_mode.refresh / 1000
+            ))
+            .output()
+            .expect("failed to execute process");
+    }
+
+    pub fn onoff(&self, on: bool) {
+        let command = match on {
+            true => "enable",
+            false => "disable",
+        };
+        Command::new("swaymsg")
+            .arg("output")
+            .arg(&self.name)
+            .arg(command)
+            .output()
+            .expect("failed to execute process");
+    }
+
+    pub fn is_current_mode(&self, mode: Mode) -> bool {
+        match &self.current_mode {
+            Some(current_mode) => {
+                current_mode.height == mode.height
+                    && mode.width == current_mode.width
+                    && mode.refresh == current_mode.refresh
+            }
+            None => false,
+        }
+    }
 }
 
-pub fn set_output(display_mode: (Display, Mode)) {
-    Command::new("swaymsg")
-        .arg("output")
-        .arg(display_mode.0.name)
-        .arg("pos")
-        .arg("0")
-        .arg("0")
-        .arg("res")
-        .arg(format!("{}x{}", display_mode.1.width, display_mode.1.height))
-        .output()
-        .expect("failed to execute process");
-}
-
-pub fn get_displays() -> Vec<Display> {
+pub fn get_displays() -> HashMap<String, Display> {
     let output = Command::new("swaymsg")
         .arg("-t")
         .arg("get_outputs")
@@ -52,5 +78,6 @@ pub fn get_displays() -> Vec<Display> {
         .expect("failed to execute process")
         .stdout;
     let output_str = str::from_utf8(&output).unwrap();
-    serde_json::from_str(output_str).unwrap()
+    let displays: Vec<Display> = serde_json::from_str(output_str).unwrap();
+    displays.into_iter().map(|d| (d.name.clone(), d)).collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,108 @@
 mod display;
 
-use cursive::views::{Dialog, LinearLayout, RadioGroup};
-use display::{get_display_modes, set_output, Display, Mode};
+use cursive::view::Nameable;
+use cursive::views::{Checkbox, Dialog, LinearLayout, RadioGroup, TextView, ViewRef};
+use display::{get_displays, Display, Mode};
+use std::collections::HashMap;
 
 fn main() {
-    let displays: Vec<(Display, Mode)> = get_display_modes();
-    let displays_for_buttons = displays.clone();
+    let displays = get_displays();
     // Creates the cursive root - required for every application.
     let mut siv = cursive::default();
-
+    siv.set_user_data(displays.clone());
     siv.add_global_callback('q', |s| s.quit());
 
-    let mut display_group = RadioGroup::<(Display, Mode)>::new();
     let mut linear = LinearLayout::vertical();
 
-    for display in displays {
-        let current_mode = display.1.clone();
-        let display_name = format!(
-            "{} {}x{} - {}hz",
-            display.0.name, current_mode.width, current_mode.height, current_mode.refresh / 1000
-        );
-        let mut button = display_group.button(display.clone(), display_name);
-        match display.0.current_mode {
-            Some(mode) => {
-                if mode.height == current_mode.height && mode.width == current_mode.width && mode.refresh == current_mode.refresh {
-                   button.select();
-                }
-            },
-            None => {}
+    for (display_name, display) in displays {
+        let display_group = RadioGroup::<Mode>::new();
+        let mut display_horizontal_layout = LinearLayout::horizontal();
+        let mut display_clone = display.clone();
+        let mut vertical_linear = LinearLayout::vertical().with_name(display_clone.name.clone());
+        let name = display_clone.name.clone();
+        display_horizontal_layout.add_child(TextView::new(name));
+        if display.active {
+            add_modes(
+                &mut vertical_linear.get_mut(),
+                &mut display_clone,
+                display_group.clone(),
+            );
         }
-        linear.add_child(button);
+        let checkbox_display_group = display_group.clone();
+        let checkbox = Checkbox::new()
+            .with_checked(display_clone.active)
+            .on_change(move |s, checked| {
+                let user_data = s
+                    .with_user_data(|user_data: &mut HashMap<String, Display>| user_data.clone())
+                    .unwrap();
+                let selected_display_first = &user_data[&display_name];
+                let mut view: ViewRef<LinearLayout> =
+                    s.find_name(&selected_display_first.name).unwrap();
+                s.with_user_data(|user_data: &mut HashMap<String, Display>| {
+                    let mut selected_display = &mut user_data.get_mut(&display_name).unwrap();
+                    match checked {
+                        true => {
+                            selected_display.active = true;
+                            add_modes(&mut *view, selected_display, checkbox_display_group.clone());
+                        }
+                        false => {
+                            selected_display.active = false;
+                            view.clear();
+                        }
+                    }
+                })
+                .unwrap()
+            });
+        display_horizontal_layout.add_child(checkbox);
+        linear.add_child(display_horizontal_layout);
+        linear.add_child(vertical_linear);
     }
 
     siv.add_layer(
         Dialog::new()
             .content(linear)
             .title("Displays")
-            .button("Set display", move |_s| {
-                let display_mode = displays_for_buttons[display_group.selected_id()].clone();
-                set_output(display_mode);
+            .button("Set display", |s| {
+                s.with_user_data(|user_data: &mut HashMap<String, Display>| {
+                    for display in user_data.values() {
+                        let display_loop = display.clone();
+                        display_loop.process();
+                    }
+                });
             })
             .button("Quit", |s| s.quit()),
     );
 
     // Starts the event loop.
     siv.run();
+}
+
+fn add_modes(
+    vertical_linear: &mut LinearLayout,
+    display: &mut Display,
+    mut display_group: RadioGroup<Mode>,
+) {
+    for mode in &display.modes {
+        let display_name = format!(
+            "{} {}x{} - {}hz",
+            display.name,
+            mode.width,
+            mode.height,
+            mode.refresh / 1000
+        );
+
+        let mut button = display_group.button(mode.clone(), display_name.clone());
+        let mode_clone = mode.clone();
+        if display.clone().is_current_mode(mode_clone) {
+            button.select();
+        }
+        vertical_linear.add_child(button);
+    }
+    let display_name = display.name.clone();
+    display_group.set_on_change(move |s, display_mode: &Mode| {
+        s.with_user_data(|user_data: &mut HashMap<String, Display>| {
+            let mut selected_display = user_data.get_mut(&display_name).unwrap();
+            selected_display.current_mode = Some(display_mode.clone());
+        });
+    });
 }
